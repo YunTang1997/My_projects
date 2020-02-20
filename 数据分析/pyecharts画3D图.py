72c777003ce9111e68c5894e96d241e655f17559
YunTang1997 <2074402078@qq.com>
2020-02-20 11:59:36 +0800

    pyecharts画3D图，增添3D散点图、曲面图部分

--- 数据分析/pyecharts画3D图.py
+++ 数据分析/pyecharts画3D图.py
@@ -2,6 +2,8 @@ import random
 import math
 from pyecharts.charts import Bar3D
 from pyecharts.charts import Line3D
+from pyecharts.charts import Scatter3D
+from pyecharts.charts import Surface3D
 from pyecharts import options as opts
 from pyecharts.faker import Faker
 
@@ -81,3 +83,87 @@ def line3d_base():
 
 
 line3d_base().render(path="C:\\Users\\tangyun\\Desktop\\生成图像\\figure43.html")
+
+
+def scatter3d_base():
+    data = [
+        [random.randint(0, 100), random.randint(0, 100), random.randint(0, 100)]
+        for _ in range(80)
+    ]
+    c = (
+        Scatter3D()
+        .add("", data)
+        .set_global_opts(
+            title_opts=opts.TitleOpts("Scatter3D-基本示例"),
+            visualmap_opts=opts.VisualMapOpts(range_color=Faker.visual_color),
+        )
+    )
+    return c
+
+
+scatter3d_base().render(path="C:\\Users\\tangyun\\Desktop\\生成图像\\figure44.html")
+
+
+def surface3d_base():
+    def surface3d_data():
+        for t0 in range(-60, 60, 1):
+            y = t0 / 60
+            for t1 in range(-60, 60, 1):
+                x = t1 / 60
+                if math.fabs(x) < 0.1 and math.fabs(y) < 0.1:
+                    z = "-"
+                else:
+                    z = math.sin(x * math.pi) * math.sin(y * math.pi)
+                yield [x, y, z]
+
+    c = (
+        Surface3D()
+        .add(
+            "",
+            list(surface3d_data()),
+            xaxis3d_opts=opts.Axis3DOpts(type_="value"),
+            yaxis3d_opts=opts.Axis3DOpts(type_="value"),
+            grid3d_opts=opts.Grid3DOpts(width=100, height=100, depth=100),
+        )
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title="Surface3D-基本示例"),
+            visualmap_opts=opts.VisualMapOpts(
+                max_=3, min_=-3, range_color=Faker.visual_color
+            ),
+        )
+    )
+    return c
+
+
+surface3d_base().render(path="C:\\Users\\tangyun\\Desktop\\生成图像\\figure45.html")
+
+
+def surface3d_flower():
+    def surface3d_data():
+        for t0 in range(-30, 30, 1):
+            y = t0 / 10
+            for t1 in range(-30, 30, 1):
+                x = t1 / 10
+                z = math.sin(x * x + y * y) * x / 3.14
+                yield [x, y, z]
+
+    c = (
+        Surface3D()
+        .add(
+            "",
+            list(surface3d_data()),
+            xaxis3d_opts=opts.Axis3DOpts(type_="value"),
+            yaxis3d_opts=opts.Axis3DOpts(type_="value"),
+            grid3d_opts=opts.Grid3DOpts(width=100, height=100, depth=100),
+        )
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title="Surface3D-曲面波图"),
+            visualmap_opts=opts.VisualMapOpts(
+                max_=1, min_=-1, range_color=Faker.visual_color
+            ),
+        )
+    )
+    return c
+
+
+surface3d_flower().render(path="C:\\Users\\tangyun\\Desktop\\生成图像\\figure46.html")
