93ee2992b9b98eedd1f1fc7d30ff3470e00f3f60
YunTang1997 <2074402078@qq.com>
2020-04-20 22:49:45 +0800

    牛客网编程输入、输出规范

---
+++ 基础练习/单行多行输入输出规范.py
@@ -0,0 +1,17 @@
+# coding = utf-8
+
+"""
+Author: YunTang
+version: 0.1
+date: 2020/4/15
+desc: 牛客网编程题输出规范
+"""
+
+import sys
+
+# 单行多行输入输出规范示例
+for line in sys.stdin:
+    a = line.split()
+    print(int(a[0]) + int(a[1]))
+
+
---
+++ 基础练习/多行输入输出规范.py
@@ -0,0 +1,24 @@
+# coding = utf-8
+
+"""
+Author: YunTang
+version: 0.1
+date: 2020/4/15
+desc: 牛客网编程题输出规范
+"""
+
+import sys
+
+
+if __name__ == "__main__":
+    # 读取第一行的n
+    n = int(sys.stdin.readline().strip())
+    ans = 0
+    for i in range(n):
+        # 读取每一行
+        line = sys.stdin.readline().strip()
+        # 把每一行的数字分隔后转化成int列表
+        values = list(map(int, line.split()))
+        for v in values:
+            ans += v
+    print(ans)
