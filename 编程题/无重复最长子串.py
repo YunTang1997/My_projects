8d6fec8662c507677e415963637945f0c6645a96
YunTang1997 <2074402078@qq.com>
2020-04-30 22:25:37 +0800

    无重复最长子串

---
+++ 编程题/无重复最长子串.py
@@ -0,0 +1,36 @@
+# coding = utf-8
+
+"""
+Author: YunTang
+version: 0.1
+date: 2020/4/27
+desc: 利用移动窗口和哈希表（字典）解决无重复最长子串的问题
+"""
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        s_len = len(s)
+        s_dict = {}
+        for key, elem in enumerate(s):  # 将原字符串转化为索引：元素的字典，方便后面元素访问
+            s_dict[key] = elem
+
+        hasmap = {}  # 利用哈希表处理匹配过的字符
+        # i为移动窗口的开始位置，j为移动窗口的结束位置
+        i, j, ans= 0, 0, 0
+        while i <= s_len and j <= s_len:  # 注意边界值的处理
+            if j < s_len and s_dict[j] not in hasmap.keys():  # 前后两个判断条件顺序不能更改，否则s_dict[j]索引溢出
+                hasmap[s_dict[j]] = j  # 将键值对加入字典
+                j += 1  # 更新j值
+            else:
+                ans = max(ans, j - i)  # 得到符合要求的子串长度
+                if j < s_len:  # 限定j的范围
+                    if hasmap.get(s_dict[j]) >= i:  # 注意判断hasmap中重复键值与i的关系
+                        i = hasmap.get(s_dict[j]) + 1  # 更新i的值（移动窗口）
+                    hasmap[s_dict[j]] = j  # 更新hasmap中重复元素的值
+                j += 1  # 更新j值
+        return ans
+
+
+print(Solution().lengthOfLongestSubstring("abba"))
+
+
