53b28a532f32e5ba663ac48c9900392c9dc259d0
YunTang1997 <2074402078@qq.com>
2020-05-22 15:28:02 +0800

    插入排序的第二种写法

--- 数据结构与算法/插入排序.py
+++ 数据结构与算法/插入排序.py
@@ -22,4 +22,17 @@ def list_sort(lst):
     return lst
 
 
-print(list_sort([1, 2, 3, 4, 0, -1, -2, 10, 9]))
+def insert_sort(alist):
+    n = len(alist)
+    for i in range(1, n):  # 右边无序序列中会有多少个元素被取出
+        for j in range(i, 0, -1):  # 从右边的无序序列中取出第一个元素，即i的位置的元素，然后将其插入到左边有序序列正确的位置中
+            if alist[j - 1] > alist[j]:
+                alist[j - 1], alist[j] = alist[j], alist[j - 1]
+            else:  # 优化算法
+                break
+    return alist
+
+
+if __name__ == '__main__':
+    print(list_sort([1, 2, 3, 4, 0, -1, -2, 10, 9]))
+    print(insert_sort([1, 2, 3, 4, 0, -1, -2, 10, 9]))
