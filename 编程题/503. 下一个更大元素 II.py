0d217534b212dc123ccc26bda309de59a6009685
YunTang1997 <2074402078@qq.com>
2020-06-13 22:38:43 +0800

    503.下一个更大元素II（修改）

--- 编程题/503. 下一个更大元素 II.py
+++ 编程题/503. 下一个更大元素 II.py
@@ -8,22 +8,38 @@ desc: 给定一个循环数组（最后一个元素的下一个元素是数组�
 """
 
 
-def nextGreaterElements(nums):
+def nextGreaterElements_1(nums):
     n = len(nums)
     res =[-1 for _ in range(n)]
     stack = []
     for i in range(2 * n):
         while stack and nums[i % n] > nums[stack[-1]]:
-            '''if res[stack[-1]] == -1:
+            '''if res[stack[-1]] == -1:  # 没有更新过的值需要更新
                 res[stack.pop()] = nums[i % n]
-            else:
+            else:  # 更新过的值不需要再更新
                 stack.pop()'''
             res[stack.pop()] = nums[i % n]
         stack.append(i % n)
     return res
 
 
+def nextGreaterElements_2(nums):
+    n = len(nums)
+    res = [-1 for _ in range(n)]
+    stack = []
+    for i in range(2 * n - 1, -1, -1):
+        while stack and nums[i % n] >= nums[stack[-1]]:
+            stack.pop()
+        if stack:
+            res[i % n] = nums[stack[-1]]
+        stack.append(i % n)
+    return res
+
+
 if __name__ == '__main__':
-    print(nextGreaterElements([1,2,1]))
-    print(nextGreaterElements([73, 74, 75, 71, 69, 72, 76, 73]))
-    print(nextGreaterElements([100, 1, 11, 1, 120, 111, 123, 1, -1, -100]))
+    print(nextGreaterElements_1([1,2,1]))
+    print(nextGreaterElements_1([73, 74, 75, 71, 69, 72, 76, 73]))
+    print(nextGreaterElements_1([100, 1, 11, 1, 120, 111, 123, 1, -1, -100]))
+    print(nextGreaterElements_2([1,2,1]))
+    print(nextGreaterElements_2([73, 74, 75, 71, 69, 72, 76, 73]))
+    print(nextGreaterElements_2([100, 1, 11, 1, 120, 111, 123, 1, -1, -100]))
